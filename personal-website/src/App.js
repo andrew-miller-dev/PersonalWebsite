d886a75828841be608ac2394eb46c70a15c9ce1d
Andrew Miller <55397229+andrew-miller182@users.noreply.github.com>
2021-06-30 20:16:25 -0600

    app creation

---
+++ personal-website/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Andrew Miller</h1>
+      </header>
+      <div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
